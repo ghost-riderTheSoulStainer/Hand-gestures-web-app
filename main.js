e547089f48498115a2e1cd31b103629c7f1360c3
ghost-riderTheSoulStainer <74955611+ghost-riderTheSoulStainer@users.noreply.github.com>
2021-10-29 21:01:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ Webcam.set({
     width:350,
     height:300,
     img_format:'png',
-    png_quality=90
+    png_quality:90
 });
 Webcam.attach('#camera');
 function take_snapshot(){
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"'
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">';
     });
 }
 console.log("ml5 version:",ml5.version);
-classifier=ml5.imageclassifier('https://teachablemachine.withgoogle.com/models/tokN0cIY1/model.json',modelloaded());
-function modlloaded(){
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/tokN0cIY1/model.json',modelloaded);
+function modelloaded(){
     console.log("model is loded");
 }
 function speak(){
